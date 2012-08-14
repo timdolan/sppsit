9dcd992b700561ecc469dde42e92ecea547ce1ed
Tim Dolan <timothy.dolan@spps.org>
2012-08-14 12:49:45 -0500

    updated cap recipe

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,15 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "it"
+set :repository,  "git@github.com:timdolan/it.spps.org.git" # Your clone URL
+set :deploy_to, "/var/www/it"
+set :user, "tim"
+set :branch, "master"
 
-set :scm, :subversion
+
+set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server '10.3.5.243', :web, :app, :db, :primary => true
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +18,10 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
